e6ce5dc3b8e8c35251f663fc69461226cdd93224
mrnakumar <nknkchoudhary@gmail.com>
2019-11-07 18:37:16 +0100

    updatd about js

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,6 @@ import Layout from "../components/layout"
 
 export default ({ data }) => (
   <Layout>
-    <h1>About {data.site.siteMetadata.title}</h1>
     <p>
       Welcome to my blog, my name is Narendra Kumar. Here I document what i learn.
     </p>
